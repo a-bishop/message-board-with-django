824e194489ca2319e6c1bb9f736abdd7a0a43e82
Andrew Bishop <andrew.bishop53@gmail.com>
2019-02-10 17:06:34 -0800

    api post requests handled

--- api_server/controllers/msg-api.js
+++ api_server/controllers/msg-api.js
@@ -3,12 +3,21 @@ const messageModel = mongoose.model('message');
 
 // GET Request Handler
 const getAllMessagesOrderedByLastPosted = (req, res) => {
-  res.status(200).send('Successful API GET Request');
+  res.status(200).send('Successful API POST Request');
 };
 
 // POST Request Handler
 const addNewMessage = (req, res) => {
-  res.status(200).send('Successful API POST Request');
+  messageModel
+  .find()
+  .sort( {'_id': 'desc'} )
+  .exec( (err, messages) => {
+    if (err) {
+      res.status(404).json(err);
+    } else {
+      res.status(200).json(messages);
+    }
+  });
 };
 
 module.exports = {
